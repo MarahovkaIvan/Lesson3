27d0a0dab90e276fb82de39a167d9155f79763d5
Ivan <ivanmarahovka@server.local>
2013-10-31 20:25:07 +0600

    Initial Commit

---
+++ Lesson3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lesson3
+//
+//  Created by Иван Мараховка on 31.10.13.
+//  Copyright (c) 2013 Иван Мараховка. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Lesson3Tests/Lesson3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Lesson3Tests.m
+//  Lesson3Tests
+//
+//  Created by Иван Мараховка on 31.10.13.
+//  Copyright (c) 2013 Иван Мараховка. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Lesson3Tests : XCTestCase
+
+@end
+
+@implementation Lesson3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
